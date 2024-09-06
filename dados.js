e8a5ea90c32ad5d1d8511bc26e3daa78daf55909
Mário Lúcio <marioluciofjr@gmail.com>
2024-09-06 09:48:50 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,45 @@
+let dados = [
+    {
+        titulo: "DOPAMINA",
+        descricao: "Efeitos na vida: Motivação e energia, melhora a ansiedade e promove ânimo. <br>Práticas Diárias: Exercícios físicos, expressar gratidão e vibrar com conquistas.",
+        link: "https://www.tuasaude.com/dopamina/",
+        tags: "motivação energia ansiedade"
+    },
+    {
+        titulo: "SEROTONINA",
+        descricao: "Efeitos na vida: Promove felicidade e bem-estar, equilíbrio do humor e regula o ciclo do sono. <br>Práticas Diárias: Demonstrar otimismo.",
+        link: "https://www.tuasaude.com/serotonina/",
+        tags: "sono felicidade humor"
+    },
+    {
+        titulo: "MELATONINA",
+        descricao: "Efeitos na vida: Melhora a qualidade do sono e qualidade de vida. <br>Práticas Diárias: Tomar sol regularmente e dormir com ambiente sempre escuro e calmo.",
+        link: "https://www.tuasaude.com/melatonina/",
+        tags: "sono sol qualidade de vida"
+    },
+    {
+        titulo: "NORADRENALINA",
+        descricao: "Efeitos na vida: Reduz o estresse, melhora o foco e capacidade de pensar com clareza. Criatividade e memória. <br>Práticas Diárias: Se exercitar e receber massagens.",
+        link: "https://www.tuasaude.com/noradrenalina/",
+        tags: "exercício foco estresse"
+    },
+    {
+        titulo: "OXITOCINA",
+        descricao: "Efeitos na vida: Promove conexão, confiança, bons sentimentos e amor. <br>Práticas Diárias: Abraços, afetos, dançar, receber carinho e aconchego.",
+        link: "https://www.tuasaude.com/es/oxitocina/",
+        tags: "abraços afetos carinho"
+    },
+    {
+        titulo: "GABA",
+        descricao: "Efeitos na vida: Melhora o foco mental, reduz ansiedade e promove relaxamento. <br>Práticas Diárias: Meditação, yoga, respiração e exercícios físicos.",
+        link: "https://www.essentialnutrition.com.br/conteudos/gaba/",
+        tags: "yoga foco relaxamento"
+    },
+    {
+        titulo: "ENDORFINA",
+        descricao: "Efeitos na vida: Aumenta a sensação de felicidade e bem-estar, melhora o sistema imunológico e a saúde física e mental. <br>Práticas Diárias: Contato com a natureza, sorrisos e chocolate.",
+        link: "https://www.tuasaude.com/como-aumentar-endorfina/",
+        tags: "bem-estar saúde chocolate"
+    }
+
+];
