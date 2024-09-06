356566ca0e7515d28281e40d3169ef7c9254c38d
Mário Lúcio <marioluciofjr@gmail.com>
2024-09-06 10:45:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
+// Função chamada ao clicar no botão "Pesquisar"
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
+    // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    // Captura o valor digitado pelo usuário no campo de pesquisa
     let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-    // se campoPesquisa for uma string sem nada
+    // Se o campo de pesquisa estiver vazio, exibe uma mensagem de erro
     if (!campoPesquisa) {
-        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um neurotrasnmissor ou sentimento</p>"
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um neurotransmissor ou sentimento</p>"
         return
     }
 
+    // Converte o valor do campo de pesquisa para minúsculas
     campoPesquisa = campoPesquisa.toLowerCase()
 
-    // Inicializa uma string vazia para armazenar os resultados
+    // Inicializa variáveis para armazenar os resultados da pesquisa
     let resultados = "";
     let titulo = "";
     let descricao = "";
     let tags = "";
 
-    // Itera sobre cada dado da lista de dados
+    // Itera sobre a lista de dados para encontrar correspondências
     for (let dado of dados) {
+        // Converte título, descrição e tags para minúsculas para comparação
         titulo = dado.titulo.toLowerCase()
         descricao = dado.descricao.toLowerCase()
         tags = dado.tags.toLowerCase()
-        // se titulo includes campoPesquisa
+
+        // Verifica se o termo pesquisado está presente no título, descrição ou tags
         if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-            // cria um novo elemento
+            // Cria um novo elemento com os resultados encontrados
             resultados += `
             <div class="item-resultado">
                 <h2>
@@ -38,10 +43,11 @@ function pesquisar() {
         }
     }
 
+    // Se nenhum resultado for encontrado, exibe uma mensagem
     if (!resultados) {
         resultados = "<p>Nada foi encontrado</p>"
     }
 
-    // Atribui os resultados gerados à seção HTML
+    // Insere os resultados na seção HTML
     section.innerHTML = resultados;
 }
